ce5e10ff665f4cfcfd64bdf3f9a18cdca58f3c69
vinicius kamiya <vinisvkami@gmail.com>
2021-11-18 12:39:12 -0400

    em furia

--- YouTank/Robomba.cpp
+++ YouTank/Robomba.cpp
@@ -8,8 +8,30 @@ Robomba::Robomba()
 	setSize(220.f,300.f);
 	body.setOrigin(body.getSize() / 2.0f);
 	setPosition(1150.f, 150.f);
+	vida = 1000;
 }
 
 Robomba::~Robomba()
 {
+}
+
+const bool Robomba::EmFuria() const
+{
+	if (vida < vida / 2)
+		return true;
+}
+
+void Robomba::tomarDano(int x)
+{
+	vida -= x;
+}
+
+void Robomba::curaVida(float x)
+{
+	vida += x;
+}
+
+const bool Robomba::getVida() const
+{
+	return vida;
 }
